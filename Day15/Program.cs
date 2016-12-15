02332574356f0a6644ea7f5e2814cc7dd1a48a40
Martijn Bodewes <martijn.bodewes@mzh.nl>
2016-12-15 11:31:52 +0100

    Code clean up

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -10,46 +10,49 @@ namespace Day15
     {
         static void Main(string[] args)
         {
-            Timing t = new Timing();
-            t.run();
+            Timing example = new Timing(
+                new Func<int, int>[]{
+                    t => (4 + t)%5,
+                    t => (1 + t)%2
+                });
+            example.run();
+
+            Timing part1 = new Timing(
+                new Func<int, int>[]{
+                    t => (1  + t)%13,
+                    t => (10 + t)%19,
+                    t => (2  + t)%3,
+                    t => (1  + t)%7,
+                    t => (3  + t)%5,
+                    t => (5  + t)%17
+                });
+            part1.run();
+
+            Timing part2 = new Timing(
+                new Func<int, int>[]{
+                    t => (1  + t)%13,
+                    t => (10 + t)%19,
+                    t => (2  + t)%3,
+                    t => (1  + t)%7,
+                    t => (3  + t)%5,
+                    t => (5  + t)%17,
+                    t => (0  + t)% 11
+                });
+            part2.run();
+
             Console.ReadLine();
         }
     }
 
     public class Timing
     {
-
-        // disc functions.
-        //Example
-        //Func<int, int>[] discs = new Func<int, int>[]{
-        //    t => (4 + t)%5,
-        //    t => (1 + t)%2
-        //};
-
-        // part 1
-        // t => (disc start pos + t ) % positions
-        //Func<int, int>[] discs = new Func<int, int>[]{
-        //    t => (1  + t)%13,
-        //    t => (10 + t)%19,
-        //    t => (2  + t)%3,
-        //    t => (1  + t)%7,
-        //    t => (3  + t)%5,
-        //    t => (5  + t)%17
-        //};
-
-
-        // part 2
-        // t => (disc start pos + t ) % positions
-        Func<int, int>[] discs = new Func<int, int>[]{
-            t => (1  + t)%13,
-            t => (10 + t)%19,
-            t => (2  + t)%3,
-            t => (1  + t)%7,
-            t => (3  + t)%5,
-            t => (5  + t)%17,
-            t => (0  + t)% 11
-        };
-
+        // Array of Disc functions:
+        // Function: f(time) => (disc_start_pos + time ) % positions
+        private Func<int, int>[] discs;
+        public Timing(Func<int, int>[] input)
+        {
+            discs = input;
+        }
 
 
         public void run()
@@ -57,8 +60,8 @@ namespace Day15
             int timer = 0;
             while (true)
             {
-                if (timer % 1000 == 0)
-                    Console.WriteLine("Checking: {0}", timer);
+                //if (timer % 1000 == 0)
+                //    Console.WriteLine("Checking: {0}", timer);
                 bool done = true;
                 for (int disc = 0; disc < discs.Count(); disc++)
                 {
